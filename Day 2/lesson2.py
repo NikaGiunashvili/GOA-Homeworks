5fadfdbf8d432f9e25f1718e043c9cee8318ae48
NikaGiunashvili <nikagiunashvili120@gmail.com>
2025-02-05 04:44:36 -0800

    second commit

---
+++ Day 2/lesson2.py
@@ -0,0 +1,17 @@
+name = "Im Nika" #str
+surname = "My Surname Is Giunashvili"
+age = "Im 13 Years Old" #int
+height = "My Height Is 158" #float
+knows_programing = "I kinda Know Programming"
+Is_Ugly = "Im Not Ugly"
+
+# print(type(name))
+# print(type(surname))
+# print(type(age))
+# print(type(height))
+# print(type(knows_programing))
+# print(type(Is_Ugly))
+
+print(name + " " + surname)
+print(age + " " + height)
+print(knows_programing + " " + Is_Ugly)
