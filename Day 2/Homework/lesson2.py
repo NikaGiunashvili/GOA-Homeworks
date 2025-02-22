7b485e15f2bea3413e0d3e76140604e97d686dcb
NikaGiunashvili <nikagiunashvili120@gmail.com>
2025-02-22 01:31:00 -0800

    second commit

---
+++ Day 2/Homework/lesson2.py
@@ -0,0 +1,39 @@
+#მიმატება
+print (10 + 10)
+print (4 + 7)
+print (6 + 9)
+print (5 + 5)
+print (8 + 7)
+
+#გამოკლება
+print (9 - 6)
+print (10 - 7)
+print (68 - 30)
+print (4 - 2)
+print (35 - 13)
+
+#გამრავლება
+
+print(10 * 10)
+print(7 * 8)
+print(5 * 5)
+print(4 * 2)
+print(7 *5)
+
+#გაყოფა
+
+print(50 // 10)
+print(4 // 2)
+print(100 // 10)
+print(9 // 3)
+print(10 // 10)
+
+
+print(10 / 10)
+print(50 / 2)
+
+print(50 % 10)
+print(100 % 10)
+print(35 % 10)
+
+
